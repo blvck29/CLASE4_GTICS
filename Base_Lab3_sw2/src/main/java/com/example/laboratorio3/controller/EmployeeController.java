6bd6cc99b3e9fd6d075c184ba30686be47b356de
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-19 20:29:11 -0500

    employeeRepository

--- Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,13 +1,21 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.repository.EmployeesRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
 
+    final EmployeesRepository employeesRepository;
+
+    public EmployeeController(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
+    }
+
     @GetMapping(value = "/lista_empleados")
     public String listaEmployee(   ){
+
         return "/employee/lista";
     }
 
